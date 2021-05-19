78f49706c518d2b8de0e177d05db056d184f52fe
jnyou <1447177020@qq.com>
2021-05-19 23:00:16 +0800

    init platform module

---
+++ cloud-member/src/main/java/io/commchina/cloudmember/CloudMemberApplication.java
@@ -0,0 +1,13 @@
+package io.commchina.cloudmember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudMemberApplication.class, args);
+    }
+
+}
