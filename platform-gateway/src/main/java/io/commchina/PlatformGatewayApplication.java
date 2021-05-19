9f0e51166749ae1a3cf7beaede5e92fad6c9fb33
jnyou <1447177020@qq.com>
2021-05-19 17:28:07 +0800

    init cloud-coding

---
+++ platform-gateway/src/main/java/io/commchina/PlatformGatewayApplication.java
@@ -0,0 +1,13 @@
+package io.commchina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformGatewayApplication.class, args);
+    }
+
+}
