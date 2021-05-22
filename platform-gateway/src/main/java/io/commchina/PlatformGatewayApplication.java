1c45489d390c1542e5f862200eeaa9f73668f9be
jnyou <1447177020@qq.com>
2021-05-22 19:49:01 +0800

    新增网关的基本配置信息

--- platform-gateway/src/main/java/io/commchina/PlatformGatewayApplication.java
+++ platform-gateway/src/main/java/io/commchina/PlatformGatewayApplication.java
@@ -2,8 +2,10 @@ package io.commchina;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PlatformGatewayApplication {
 
     public static void main(String[] args) {
---
+++ platform-gateway/src/main/java/io/commchina/controller/GatewayRulesController.java
@@ -0,0 +1,40 @@
+package io.commchina.controller;
+
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiDefinition;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.GatewayApiDefinitionManager;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+/**
+ * 代码千万行，注释第一行
+ * 注释不规范，同事泪两行
+ * <p>
+ * GatewayRulesController
+ * 网关限流策略
+ *
+ * @version 1.0.0
+ * @author: JnYou
+ **/
+@RestController
+public class GatewayRulesController {
+
+    /**
+     * 获取当前系统的限流策略
+     */
+    @GetMapping("/gw/flow/rules")
+    public Set<GatewayFlowRule> getCurrentGatewayFlowRules() {
+        return GatewayRuleManager.getRules();
+    }
+
+    /**
+     * 获取我定义的api分组
+     */
+    @GetMapping("/gw/api/groups")
+    public Set<ApiDefinition> getApiGroups() {
+        return GatewayApiDefinitionManager.getApiDefinitions();
+    }
+}
